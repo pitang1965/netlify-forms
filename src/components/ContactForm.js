e923c478cee40ac5f1e9b8dc652311a5a8d0033a
Shoji Makino <pitang1965@gmail.com>
2021-06-19 23:28:28 +0900

    Add styling

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import * as styles from './ContactForm.module.css';
 
 function ContactForm() {
   const [formState, setFormState] = useState({
@@ -36,6 +37,7 @@ function ContactForm() {
 
   return (
     <form
+      className={styles.form}
       onSubmit={handleSubmit}
       name="contact"
       method="post"
@@ -43,33 +45,49 @@ function ContactForm() {
       data-netlify-honeypot="bot-field"
     >
       <input type="hidden" name="form-name" value="contact" />
-      <label htmlFor="name">名前</label>
-      <input
-        id="name"
-        type="text"
-        name="name"
-        onChange={handleChange}
-        value={formState.name}
-        placeholder="名前を入力してください。"
-      />
-      <label htmlFor="email">メールアドレス</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        onChange={handleChange}
-        value={formState.email}
-        placeholder="メールアドレスを入力してください。"
-      />
-      <label htmlFor="message">メッセージ</label>
-      <textarea
-        id="message"
-        name="message"
-        value={formState.message}
-        onChange={handleChange}
-        placeholder="メッセージを入力してください。"
-      />
-      <button type="submit">送信</button>
+      <label className={styles.title}>お問い合わせフォーム</label>
+      <label className={styles.description}>
+        お気軽にお問い合わせください。24時間以内に返信いたします。
+      </label>
+      <fieldset>
+        <label htmlFor="name">名前</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={formState.name}
+          placeholder="名前を入力してください。"
+          required
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="email">メールアドレス</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={formState.email}
+          placeholder="メールアドレスを入力してください。"
+          required
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="message">メッセージ</label>
+        <textarea
+          className={styles.message}
+          id="message"
+          name="message"
+          value={formState.message}
+          onChange={handleChange}
+          placeholder="メッセージを入力してください。"
+          required
+        />
+      </fieldset>
+      <fieldset>
+        <button type="submit">送信</button>
+      </fieldset>
     </form>
   );
 }
